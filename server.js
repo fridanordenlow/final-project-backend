5fdc6573bb9a73cc1a2a0067c582d69c99460b95
Frida Nordenlöw <frida.nordenlow@hotmail.com>
2023-05-29 16:23:58 +0200

    base for backend created, schemas for users and missions and corresponding endpoints

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
+import listEndpoints from "express-list-endpoints";
+
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/final-project";
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = Promise;
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
@@ -13,8 +19,207 @@ app.use(express.json());
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send(listEndpoints(app));
 });
+
+// User schema
+const { Schema } = mongoose
+
+const UserSchema = new Schema({
+  // Add email, first name, surname?
+  // Add missions connected to user?
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  // Hur definierar man att bara "mailadress-form" accepteras?
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  // username: {
+  //   type: String, 
+  //   required: true,
+  //   unique: true
+  // },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+    maxlength: 30
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString("hex")
+  }
+  // missions: {}
+});
+
+const User = mongoose.model("User", UserSchema)
+
+// Mission schema
+const MissionSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  extraInfo: {
+    type: String
+  },
+  points: {
+    type: Number,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: new Date()
+  }
+  // Not necessary yet? Stretch goal for users to post their own missions
+  // user: {
+  //   type: String,
+  //   required: true
+  // }
+});
+
+const Mission = mongoose.model("Mission", MissionSchema)
+
+// Register new users (post user to database)
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body
+  try {
+    const salt = bcrypt.genSaltSync() // Obscuring our password
+    const newUser = await new User ({
+      username: username,
+      password: bcrypt.hashSync(password, salt)
+    }).save()
+    res.status(201).json({
+      success: true,
+      message: "Registration successful",
+      response: {
+        username: newUser.username,
+        id: newUser._id,
+        accessToken: newUser.accessToken
+      }
+    })
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+      message: "User already exists",
+      response: err
+    })
+  }
+});
+
+// Log in 
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body
+  try {
+    const user = await User.findOne({username: username})
+    // Comparing the password with the password filled in
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.status(200).json({
+        success: true,
+        message: "Log in successful",
+        response: {
+          username: user.username,
+          id: user._id,
+          accessToken: user.accessToken
+        } 
+      })
+    } else {
+      res.status(400).json({
+        success: false,
+        message: "Could not login, login details do not match"
+      })
+    }
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      response: err
+    })
+  }
+});
+
+// Authenticate the user
+const authenticateUser = async (req, res, next) => {
+  const accessToken = req.header("Authorization")
+  // If we find the user's correct accessToken in the header "Authorization"
+  try {
+    const user = await User.findOne({accessToken: accessToken})
+    if (user) {
+      // Calling the next function says that all of the stuff that we get from the req/res, will be transferred to the next function with the same endpoint
+      next()
+    } else {
+    res.status(401).json({
+      success: false,
+      response: "Please log in"
+    });
+    }
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      response: err
+    })
+  }
+};
+
+app.get("/missions", authenticateUser)
+// If they "pass" this is the function that happens next()
+app.get("/missions", async (req, res) => {
+  const accessToken = req.header("Authorization")
+  try {
+    const user = await User.findOne({accessToken: accessToken})
+    if (user) {
+      const missions = await Mission.find() // .populate('user') https://mongoosejs.com/docs/populate.html
+      res.status(200).json({
+        success: true,
+        response: missions,
+        message: "Missions found"
+      })
+    } else {
+      res.status(400).json({
+        success: false,
+        response: "Error, missions could not be retrieved"
+      })
+    }
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      response: err
+    })
+  }
+});
+
+// app.post("/missions", authenticateUser) do we need authentication here now?
+app.post("/missions", async (req, res) => {
+  const { title, description, extraInfo, points } = req.body
+  // const accessToken = req.header("Authorization")
+  try {
+    // const user = await User.findOne({accessToken: accessToken})
+    const newMission = await new Mission({title, description, extraInfo, points }).save()
+    res.status(201).json({
+      success: true,
+      response: newMission,
+      message: "New mission successfully created"
+    }) 
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+      response: err,
+      message: "Error, new mission could not be created"
+    })
+  }
+});
+
 
 // Start the server
 app.listen(port, () => {
